b1e945086f3d4ac7f8706f48dc8c7cd8f3f1e55b
Parker <parker@Sebastians-MacBook-Air.local>
2022-11-23 05:35:13 -0800

    fusion con api, penultimo

--- routes/users.js
+++ routes/users.js
@@ -41,15 +41,39 @@ router.delete("/:id", async (req, res) => {
 });
 
 // get a user
-router.get("/:id", async (req, res) => {
+router.get("/", async (req, res) => {
+  const userId = req.query.userId;
+  const username = req.query.username;
     try {
-      const user = await User.findById(req.params.id);
+      const user = userId 
+      ? await User.findById(userId) 
+      : await User.findOne({username:username});
       const { password, updatedAt, ...other } = user._doc;
       res.status(200).json(other);
     } catch (err) {
       res.status(500).json(err);
     }
   });
+
+// get frinds 👥
+router.get("/friends/:userId", async (req,res) =>{
+  try{
+    const user = await User.findById(req.params.userId)
+    const friends = await Promise.all(
+      user.followings.map(friendId=>{
+        return User.findById(friendId)
+      })
+    )
+    let friendList = [];
+    friends.map(friend=>{
+      const {_id, username, profilePicture} = friend;
+      friendList.push({_id, username, profilePicture});
+    });
+    res.status(200).json(friendList)
+  }catch(err){
+    res.status(500).json(err)
+  }
+})
 
 // follow a user
 
